4de9bb2d203f9d6f1cf2f7abbc993acab60e87c7
ThuQuangNguyen <quangthu1162@gmail.com>
2018-10-02 23:26:39 +0700

    create

---
+++ app/src/main/java/com/example/ibct/appdatxe/MapsActivity.java
@@ -0,0 +1,71 @@
+package com.example.ibct.appdatxe;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ProgressDialog mProgressDialog;
+    private Location myLocation;
+    private double check_location =0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+        // Add a marker in Sydney and move the camera
+            myLocation = googleMap.getMyLocation();
+
+                mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                    @Override
+                    public void onMyLocationChange(Location location) {
+                        if (check_location ==0) {
+                            LatLng myLatLng = new LatLng(location.getLatitude(),
+                                    location.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(myLatLng).title("My Location!"));
+                            CameraPosition myPosition = new CameraPosition.Builder()
+                                    .target(myLatLng).zoom(17).bearing(90).tilt(30).build();
+                            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(myPosition));
+                        }
+                        check_location += 1;
+                    }
+                });
+    }
+}
